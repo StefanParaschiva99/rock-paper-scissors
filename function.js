035d13130b58d409da6a677259cd7684b64c91b4
Stefan Paraschiva <paraschiva.stefan99@gmail.com>
2023-03-01 17:49:09 +0100

    add a function that compare the choises and return a winner. Also assigned some global variables

--- function.js
+++ function.js
@@ -1,5 +1,21 @@
-function getComputerChoise() {
-    const choises = ["Rock", "Paper", "Scissor"]
+// This function get a random choice between this 3 option and returns it
+function getComputerChoice() {
+    const choises = ["Rock", "Paper", "Scissors"]
     let choise = choises[Math.floor(Math.random()*choises.length)]
-    console.log(choise)
+    return choise
 }
+// This function will compare the choises and return a winner
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == "rock" && computerSelection == "scissors" || playerSelection == "paper" && computerSelection == "rock" || playerSelection == "scissors" && computerSelection == "paper") {
+        return ("You win!")
+    }
+    else if (playerSelection == computerSelection) {
+        return ("Tie!")
+    }
+    else { return ("You lose!") }
+   }
+
+   let playerSelection = prompt("Make your choice between: rock paper and scissors").toLowerCase();
+   const computerSelection = getComputerChoice().toLowerCase();
+   console.log(playRound(playerSelection, computerSelection))
+
